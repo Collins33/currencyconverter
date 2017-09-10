3900ff42c900bc2247f87302732d3bfc9c0927ca
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-09-10 14:13:35 +0300

    I have added code to log input to logcat

--- app/src/main/java/com/example/root/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/root/currencyconverter/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.root.currencyconverter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    //convert method will be activated by a view thats why the parameter is a view object
+    public void convert(View view){
+        //get the view(edittext)
+        EditText dollarview=(EditText) findViewById(R.id.editText);
+        //log input to logcat
+        Log.i("dollar",dollarview.getText().toString());
     }
 }
