7fa74ea2cc5d71572898ba02d9ff8661e511ac73
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-09-10 14:28:58 +0300

    I have added the toast to display the converted currency

--- app/src/main/java/com/example/root/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/root/currencyconverter/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,5 +20,12 @@ public class MainActivity extends AppCompatActivity {
         EditText dollarview=(EditText) findViewById(R.id.editText);
         //log input to logcat
         Log.i("dollar",dollarview.getText().toString());
+        //convert input to double
+        Double dollaramount=Double.parseDouble(dollarview.getText().toString());
+        //convert from dollar to ksh
+        Double poundAmount= dollaramount * 100;
+        Toast myToast= Toast.makeText(this,"ksh"+poundAmount.toString(),Toast.LENGTH_LONG);
+        myToast.show();
+
     }
 }
